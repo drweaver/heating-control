911bffed730816062a69dc5fa3dad5f3bf3705eb
Shane Weaver <sgtw_speed@yahoo.com>
2017-01-14 22:44:27 +0000

    chart optimisation for multiple channels

--- www/dashboard.js
+++ www/dashboard.js
@@ -2,97 +2,117 @@
 
 var app = angular.module('HeatingControlApp');
 
-app.controller('MainController', [ 'subscribeKey', function($scope, $http, subscribeKey) {
-  
-var __eon_pubnub = new PubNub({
-  subscribeKey: subscribeKey
-});
-var __eon_cols = ["temp"];
-var __eon_labels = {"temp":"Lounge","garage":"Garage"};
-eon.chart({
-  pubnub: __eon_pubnub,
-  channels: ["heatingcontrol.lounge.log"],
-  history: true,
-  flow: false,
-  rate: 300000,
-  limit: 288,
-  generate: {
-    bindto: "#chart",
-    data: {
-      colors: {"garage":"#113F8C","Lounge":"#E54028"},
-      type: "spline"
-    },
-    transition: {
-      duration: 250
-    },
-    tooltip: {
-     show: true
-    },
-    point: {
-      show: false
-    },
-    axis: {
-      y: { 
-        tick: { format: d3.format('.2f') },
-        max: 30,
-        min: 0,
-        padding: { top: 0, bottom: 0 }
-      },
-      x: { tick: { format: d3.time.format("%H:%M") } }
-    }
-  },
-  transform: transform_temperature
-});
+app.controller('MainController', function($scope, $http, subscribeKey) {
 
-
-eon.chart({
-  pubnub: __eon_pubnub,
-  channels: ["heatingcontrol.lounge.log"],
-  history: true,
-  limit: 1,
-  rate: 300000,
-  generate: {
-    bindto: '#lounge-gauge',
-    data: {
-      type: 'gauge'
-    },
-    gauge: {
-      min: 5,
-      max: 30,
-      units: ' C',
-      label: {
-            format: function(value, ratio) {
-                return value + ' \xB0C';
-            },
-            show: false // to turn off the min/max labels.
-      },
-    },
-    color: {
-      pattern: ['#779ECB', '#FFB347', '#FF6961'],
-      threshold: {
-        values: [18, 23]
-      }
-    }
-  },
-    transform: transform_temperature
-});
+  var __eon_pubnub = new PubNub({
+    subscribeKey: subscribeKey
+  });
+  var channels = ["heatingcontrol.lounge.log","heatingcontrol.garage.log"];
+  var __eon_labels = {"heatingcontrol.lounge.log":"Lounge","heatingcontrol.garage.log":"Garage"};
   
-var transform_temperature = function(message) {
-    console.log(message);
+  var transform_temperature = function(message) {
     var o = {};
-    var found = false;
-    for(var index in message) {
-      if(__eon_cols.indexOf(index) > -1){
-        o[__eon_labels[index] || index] = message[index];
-        found = true;
+    if( message.channel && message.temp && message.time ) {
+      if( channels.indexOf(message.channel) > -1 ) {
+        o[__eon_labels[message.channel]] = message.temp;
+        o['x'] = Math.floor(message.time/300000)*300000;
+        return { eon: o };
       }
     }
-    if( !found ) return null;
-    return {
-      eon: o
-    };
-};
-}]);
+    return null;
+  };
+  
+  eon.chart({
+    pubnub: __eon_pubnub,
+    channels: channels,
+    history: true,
+    flow: false,
+    rate: 300000,
+    limit: 288,
+    generate: {
+      bindto: "#chart",
+      data: {
+        colors: {"garage":"#113F8C","Lounge":"#E54028"},
+        type: "spline"
+      },
+      transition: { duration: 250 },
+      tooltip: {show: true },
+      point: {  show: true },
+      axis: {
+        y: { 
+          tick: { format: d3.format('.2f') }
+        },
+        x: { tick: { format: d3.time.format("%H:%M") } }
+      }
+    },
+    transform: transform_temperature,
+    xType: 'custom',
+    xId: 'x'
+  });
+  
+  
+  eon.chart({
+    pubnub: __eon_pubnub,
+    channels: ["heatingcontrol.lounge.log"],
+    history: true,
+    limit: 1,
+    rate: 300000,
+    generate: {
+      bindto: '#lounge-gauge',
+      data: { type: 'gauge' },
+      gauge: {
+        min: 5,
+        max: 30,
+        units: ' C',
+        label: {
+              format: function(value, ratio) {
+                  return value + ' \xB0C';
+              },
+              show: false // to turn off the min/max labels.
+        },
+      },
+      color: {
+        pattern: ['#779ECB', '#FFB347', '#FF6961'],
+        threshold: { values: [18, 23] }
+      }
+    },
+      transform: transform_temperature,
+      xType: 'custom',
+      xId: 'x'
+  });
+  
+  
+    eon.chart({
+    pubnub: __eon_pubnub,
+    channels: ["heatingcontrol.garage.log"],
+    history: true,
+    limit: 1,
+    rate: 300000,
+    generate: {
+      bindto: '#garage-gauge',
+      data: { type: 'gauge' },
+      gauge: {
+        min: -5,
+        max: 30,
+        units: ' C',
+        label: {
+              format: function(value, ratio) {
+                  return value + ' \xB0C';
+              },
+              show: false // to turn off the min/max labels.
+        },
+      },
+      color: {
+        pattern: ['#779ECB', '#FFB347', '#FF6961'],
+        threshold: { values: [18, 23] }
+      }
+    },
+      transform: transform_temperature,
+      xType: 'custom',
+      xId: 'x'
+  });
+
+});
 
 
 
