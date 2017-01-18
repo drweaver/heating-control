73a24dd15570be2e884e89614bb5f85f9680e049
Shane Weaver <sgtw_speed@yahoo.com>
2017-01-18 22:35:17 +0000

    tidy up file locations

---
+++ server.js
@@ -0,0 +1,13 @@
+process.title = 'heatingcontrol';
+
+require('./lib/gpio.js');
+
+require('./lib/heating.js');
+
+require('./lib/thermostat.js');
+
+require('./lib/temperature.js');
+
+require('./lib/pubnub.js');
+
+require('./lib/schedule.js');
