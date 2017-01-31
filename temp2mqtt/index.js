bbfbf064d3f69f1e5c97a38311bd75d389cd425f
Shane Weaver <sgtw_speed@yahoo.com>
2017-01-31 21:02:13 +0000

    use average

--- temp2mqtt/index.js
+++ temp2mqtt/index.js
@@ -3,23 +3,37 @@ var config = require('./etc/config.json');
 var w1temp = require('w1temp');
 var mqtt = require('mqtt');
 
+var client = mqtt.connect(config.mqtt.url);
+client.on('connect', connack=>{
+  console.log("Successfully connected to MQTT");
+});
+
+function publish(temp) {
+  client.publish(config.mqtt.topic,temp.toString(), err => {
+  if( err )
+    console.error('Failed to publish temperature reading to MQTT: '+err);
+  });
+}
+
 w1temp.getSensor(config.deviceId).then( sensor => {
-  var client = mqtt.connect(config.mqtt.url);
-  client.on('connect', connack=>{
-    console.log("Successfully connected to MQTT");
-  });
   var lastTemp = false;
-  console.log("Listening for temperature changes");
-  sensor.on('change', temp => {
-    temp = Math.round(temp*10)/10;
-    if( lastTemp !== temp ) {
-      lastTemp = temp;
-      client.publish(config.mqtt.topic,temp.toString(), err => {
-        if( err )
-          console.error('Failed to publish temperature reading to MQTT: '+err);
-      });
+  var log = [];
+  var action = () => {
+    var temp = sensor.getTemperature();
+    log.unshift(temp);
+    if( log.length > 30 ) log.pop();
+    var av = 0;
+    log.map( v => { av += v } );
+    av = Math.round((av / log.length)*10)/10;
+    if( lastTemp !== av ) {
+      lastTemp = av;
+      publish(av);
     }
-  });
+  };
+  
+  setTimeout( action, 1000);
+
 }, err => {
   console.error('Failed to get sensor: '+err);
 });
+
