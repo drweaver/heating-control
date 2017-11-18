cd70db44d49e78b177ec4bfcad2f49eb500230dd
drweaver <sgtw_speed@yahoo.com>
2017-11-18 15:21:24 +0000

    extracted config for easier containerisation

--- temp2mqtt/index.js
+++ temp2mqtt/index.js
@@ -2,14 +2,32 @@ process.title = 'temp2mqtt';
 var config = require('./etc/config.json');
 var w1temp = require('w1temp');
 var mqtt = require('mqtt');
+var _ = require('underscore');
 
-var client = mqtt.connect(config.mqtt.url, { will: { topic: config.mqtt.topic, payload: null, retain: true, qos: 0 } });
+_.each( ['TEMP_DEVICE_ID', 'TEMP_LOCATION', 'TEMP_MQTT_URL'], env=> {
+  if( !_.has(process.env, env) ) {
+    console.error('Missing environment variable: '+env+', exiting...');
+    process.exit(1);
+  }
+});
+
+var config = { 
+  deviceId: process.env.TEMP_DEVICE_ID,
+  mqtt: { 
+    topic: 'home/'+process.env.TEMP_LOCATION+'/temperature',
+    url: process.env.TEMP_MQTT_URL
+  }
+};		
+
+var client = mqtt.connect(config.mqtt.url, { will: { topic: config.mqtt.topic+'/status', payload: 'Disconnected', retain: true, qos: 0 } });
 client.on('connect', connack=>{
   console.log("Successfully connected to MQTT");
+  client.publish(config.mqtt.topic+'/status', 'OK', { retain: true });
 });
 
 function publish(temp) {
-  client.publish(config.mqtt.topic,temp.toString(), { retain: true }, err => {
+  console.log('Publishing temperature '+temp+' degrees');
+  client.publish(config.mqtt.topic,temp.toString(), { retain: false }, err => {
   if( err )
     console.error('Failed to publish temperature reading to MQTT: '+err);
   });
